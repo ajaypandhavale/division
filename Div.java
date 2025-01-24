69dc9ab6775e9355abe3626ab5fdbfb88aa7d875
ajaypandhavale <aj40135821@wipro.com>
2025-01-24 13:30:35 +0530

    adding program to repo

---
+++ Div.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Div {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		
+		System.out.println("enter two numbers to perform Division ");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		
+		int div=a/b;
+		System.out.println(" division of given numbers are "+div);
+		
+		
+
+	}
+
+}
